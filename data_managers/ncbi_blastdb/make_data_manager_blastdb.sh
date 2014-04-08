b5a65fb0d51c4772bb70073ab87e52d79fd8d29c
Michael Li <michael.li@uwaterloo.ca>
2014-04-08 14:21:48 -0400

    Added data_managers level README and updated repository build script.

--- data_managers/ncbi_blastdb/make_data_manager_blastdb.sh
+++ data_managers/ncbi_blastdb/make_data_manager_blastdb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "This will create a tar-ball suitable to upload to the toolshed."
 
-if [ -f "data_manager/data_manager_blastdb/make_data_manager_blastdb.sh" ]
+if [ -f "data_managers/ncbi_blastdb/make_data_manager_blastdb.sh" ]
 then
 echo "Good, in the expected directory"
 else
@@ -9,36 +9,36 @@ echo "ERROR. Run this from the GitHub repository root directory."
 exit 1
 fi
 
-if [ -f "data_manager_blastdb.tar.gz" ]
+if [ -f "ncbi_blastdb.tar.gz" ]
 then
-echo "ERROR. File ncbi_blast_plus.tar.gz already exists."
+echo "ERROR. File ncbi_blastdb.tar.gz already exists."
 exit 1
 fi
 
 #Create tar file with core XML wrappers
-if [ -f "data_manager_blastdb.tar" ]
+if [ -f "ncbi_blastdb.tar" ]
 then
 rm data_manager_blastdb.tar
 fi
 
 #Create tar file (-cf then -rf to add to it)
-tar -rf  data_manager_blastdb.tar test-data/cog.out
-tar -rf  data_manager_blastdb.tar test-data/pataa.out
-tar -rf  data_manager_blastdb.tar test-data/patnt.out
-tar -rf  data_manager_blastdb.tar tool-data/blastdb.loc.sample
-tar -rf  data_manager_blastdb.tar tool-data/blastdb_p.loc.sample
-tar -rf  data_manager_blastdb.tar tool-data/blastdb_d.loc.sample
-tar -rf  data_manager_blastdb.tar tool-data/tool_data_table_conf.xml.sample
-tar -rf  data_manager_blastdb.tar data_manager/data_manager_blastdb/data_manager_conf.xml
-tar -rf  data_manager_blastdb.tar data_manager/data_manager_blastdb/README
-tar -rf  data_manager_blastdb.tar data_manager/data_manager_blastdb/tool_dependencies.xml
-tar -rf  data_manager_blastdb.tar data_manager/data_manager_blastdb/blastdb.xml
-tar -rf  data_manager_blastdb.tar data_manager/data_manager_blastdb/fetch_blast_db.py
+tar -rf  ncbi_blastdb.tar test-data/cog.out
+tar -rf  ncbi_blastdb.tar test-data/pataa.out
+tar -rf  ncbi_blastdb.tar test-data/patnt.out
+tar -rf  ncbi_blastdb.tar tool-data/blastdb.loc.sample
+tar -rf  ncbi_blastdb.tar tool-data/blastdb_p.loc.sample
+tar -rf  ncbi_blastdb.tar tool-data/blastdb_d.loc.sample
+tar -rf  ncbi_blastdb.tar tool-data/tool_data_table_conf.xml.sample
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/data_manager_conf.xml
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/README.rst
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/tool_dependencies.xml
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/blastdb.xml
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/fetch_blast_db.py
 
 
 #Zip the tar file
-gzip data_manager_blastdb.tar
+gzip ncbi_blastdb.tar
 
 #Check the output
 echo "Expect a tar-ball 12 files, have:"
-tar -tzf data_manager_blastdb.tar.gz | wc -l
+tar -tzf ncbi_blastdb.tar.gz | wc -l
